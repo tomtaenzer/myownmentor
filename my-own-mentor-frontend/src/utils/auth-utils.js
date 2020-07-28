8779daa9b55a4e1bedfa6bba0b881536af5bee42
Tom Tänzer <tomtaenzer@gmx.net>
2020-07-28 16:57:28 +0200

    Add performLogin & performRegistration

--- my-own-mentor-frontend/src/utils/auth-utils.js
+++ my-own-mentor-frontend/src/utils/auth-utils.js
@@ -31,6 +31,31 @@ export function getDecodedJwtToken(){
     return decodedToken;
 }
 
-export function performLogin(){
-    //toDo
+export async function performLogin(username, password){
+    const response = await fetch('/auth/login', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(username, password),
+    });
+    if (response.status !==200) {
+        throw new Error(`Registration failed: ${response.statusText}`);
+    }
+         return await response.text();
+
+}
+
+export async function performRegistration(registrationData) {
+    const response = await fetch ('/auth/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(registrationData),
+    });
+    if (response.status !== 200) {
+        throw new Error (`Registration failed: ${response.statusText}`);
+    }
+    return await response.text();
 }
