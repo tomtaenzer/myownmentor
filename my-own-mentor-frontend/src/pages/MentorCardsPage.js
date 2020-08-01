b183284d1267acfd7725977cd25bbe6d4f11c7d0
Tom Tänzer <tomtaenzer@gmx.net>
2020-08-01 14:13:30 +0200

    Add MentorUserDrawer

---
+++ my-own-mentor-frontend/src/pages/MentorCardsPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import MentorCards from "../mentor/MentorCards";
+import MentorUserDrawer from "../components/MentorUserDrawer";
+
+
+
+
+
+
+
+export default function MentorCardsPage(){
+
+  return (
+  <MentorUserDrawer/>
+
+  )
+}
