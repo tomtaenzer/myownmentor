66039428f4e8e0cd124d67156c19a2a90a7ea76c
Tom Tänzer <tomtaenzer@gmx.net>
2020-08-01 14:09:24 +0200

    Change "Fetch of Daily match failed"

--- my-own-mentor-frontend/src/utils/mentor-utils.js
+++ my-own-mentor-frontend/src/utils/mentor-utils.js
@@ -2,7 +2,7 @@ export async function fetchAllMentorCards() {
     const response = await fetch("/api/mentorCards", {
     });
     if (response.status !== 200) {
-        throw new Error("Fetch of daily match failed")
+        throw new Error("Fetch failed")
     }
     return await response.json();
 }
